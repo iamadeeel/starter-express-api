07766699b2b5c0019cd88a84537d20b94545867f
iamadeeel <33900361+iamadeeel@users.noreply.github.com>
2023-01-26 21:01:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ app.get('/close-trade',async function (req, res) {
 });
 
 var server = app.listen(3000, function() {
-  console.log('Express server listening on port ' + port);
+  console.log('Express server listening on port 3000');
 });
 
 const timeout = 120*1000;
